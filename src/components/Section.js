536e52312a87a2feb6057c35cff0aa59e1cd06a1
Niwant <niwantsalunke308@gmail.com>
2022-05-09 20:40:17 +0530

    Added Header to the webpage

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,19 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
+
 function Section({ title, description, bgImg, leftBtnText, rightBtnText }) {
   return (
     <Wrap bg={bgImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
-      <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          <RightButton>{rightBtnText}</RightButton>
-        </ButtonGroup>
-        <DownArrow src="/images/down-arrow.svg" />
-      </Buttons>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
+      <Fade bottom>
+        <Buttons>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            <RightButton>{rightBtnText}</RightButton>
+          </ButtonGroup>
+          <DownArrow src="/images/down-arrow.svg" />
+        </Buttons>
+      </Fade>
     </Wrap>
   );
 }
@@ -21,6 +27,7 @@ function Section({ title, description, bgImg, leftBtnText, rightBtnText }) {
 export default Section;
 
 const Wrap = styled.div`
+  z-index: 10;
   width: 100vw;
   height: 100vh;
   background-size: cover;
